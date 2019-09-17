e149481f2bcc6b923a0b46cbb85eed7b5239e22d
michal.mendrek@intive.com <michal.mendrek@intive.com>
2019-09-17 16:49:19 +0200

    corrected problems with /var/log

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <log_module.h>
+#include <iostream>
+using namespace std;
+
+void funkcja_testu(std::string data, std::string name) {
+  std::cout << "ModName: " << name << "\n\rData: " << data << std::endl;
+}
+
+void funkcja_dwa(std::string data, std::string name) {
+  std::cout << "Dwa" << std::endl;
+  std::cout << "ModName: " << name << "\n\rData: " << data << std::endl;
+}
+
+int main() {
+  log_module plik("/var/log/");
+  plik.AddModule("DrowsinessModule", funkcja_testu);
+  //plik.AddModule("module2", funkcja_dwa);
+
+  //std::cout << "Module1\n\r " << plik.GetWholeLog("DrowsinessModule") << std::endl;
+  //std::cout << "Module2\n\r " << plik.GetWholeLog("module2") << std::endl;
+  while (1)
+    ;
+}
